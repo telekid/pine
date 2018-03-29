53adf3e252585d36392d5ea0cac6d7cd06c701ec
Jake Zerrer <him@jakezerrer.com>
2018-03-29 09:52:15 -0700

    Add first test

--- src/cljs/pine/core.cljs
+++ src/cljs/pine/core.cljs
@@ -1,1 +1,15 @@
 (ns pine.core)
+
+(defn match-route [path routes] false)
+
+(defn add-children [routes]
+  (reduce (fn [result [route body]]
+            (let [parent (:parent body)]
+              (loop [p parent
+                     r result]
+                (if (not (nil? p))
+                  (recur (:parent (p result))
+                         (update-in r [p :children] #(conj % route)))
+                  r))))
+          routes
+          routes))
--- test/cljs/core_test.cljs
+++ test/cljs/core_test.cljs
@@ -1,6 +1,25 @@
 (ns pine.core-test
-  (:require [cljs.test :refer-macros [deftest is testing run-tests]]))
+  (:require [cljs.test :refer-macros [deftest is testing run-tests]]
+            [pine.core :refer [match-route
+                               add-children]]))
 
-(deftest test-identity
-  (is (= 1 1)))
 
+
+(deftest test-add-children
+  (def pre-add-children
+    {:a {:duck true}
+     :b {:parent :a
+         :bear true}
+     :c {:parent :b
+         :goat true}})
+
+  (def post-add-children
+    {:a {:duck true
+         :children [:c :b]}
+     :b {:parent :a
+         :bear true
+         :children [:c]}
+     :c {:parent :b
+         :goat true}})
+
+  (is (= (add-children pre-add-children) post-add-children)))
