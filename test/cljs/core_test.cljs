76a1a0e932dcdd718558f8bae78ff6ad3bd5b9cf
Jake Zerrer <him@jakezerrer.com>
2018-04-17 14:08:59 -0700

    Remove old test cases from core_test.cljs

--- test/cljs/core_test.cljs
+++ test/cljs/core_test.cljs
@@ -2,44 +2,6 @@
   (:require [cljs.test :refer-macros [deftest is testing run-tests]]
             [pine.core :as core]))
 
-;; (deftest make-sorted
-;;   (def pre-sorted
-;;     {:a {:duck true}
-;;      :b {:parent :a
-;;          :bear true}
-;;      :c {:parent :b
-;;          :goat true}})
-
-;;   (def post-sorted
-;;     (into (sorted-map) {:a {:duck true}
-;;                         :b {:parent :a
-;;                             :bear true}
-;;                         :c {:parent :b
-;;                             :goat true}})))
-
-  ;; (is (= (core/make-sorted pre-sorted) post-sorted)))
-
-
-;; (deftest test-add-children
-;;   (def pre-add-children (core/make-sorted {:a {:duck true}
-;;                                            :b {:parent :a
-;;                                                :bear true}
-;;                                            :c {:parent :b
-;;                                                :goat true}
-;;                                            :d {:parent :a
-;;                                                :deer true}}))
-
-;;   (def post-add-children (core/make-sorted {:a {:duck true
-;;                                                 :descendents [[:b :c] [:d]]}
-;;                                             :b {:parent :a
-;;                                                 :bear true
-;;                                                 :descendents [[:b]]}
-;;                                             :c {:parent :b
-;;                                                 :goat true}})))
-
-  ;; (is (= (core/add-children pre-add-children) post-add-children)))
-
-
 (def post-nest-routes
   [{:route-id :port
     :test-path "/port"}
@@ -90,3 +52,6 @@
 
   (is (= (core/match-route "/portfolioo" post-nest-routes)
          {:active #{:portfolioo}})))
+
+
+
