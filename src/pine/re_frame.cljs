20c9c1d0aad6f1dbe9dbc267ce9ceab152423805
Jake Zerrer <him@jakezerrer.com>
2018-05-03 15:20:18 -0700

    Add standard re-frame events

--- src/pine/re_frame.cljs
+++ src/pine/re_frame.cljs
@@ -1,11 +1,15 @@
 (ns pine.re-frame
   (:require [re-frame.core :as re-frame]
+            [clojure.data :as data]
             [pine.router :as router]))
+
+
+;; -- Subscriptions ------------------------------------------------------------
 
 (re-frame/reg-sub
  :pine/location
  (fn [db]
-   (:location db)))
+   (:pine/location db)))
 
 (re-frame/reg-sub
  :pine/active-routes
@@ -18,3 +22,28 @@
  :<- [:pine/location]
  (fn [location _]
    (:params location)))
+
+
+;; -- Events ------------------------------------------------------------------
+
+(re-frame/reg-event-fx
+ :pine/navigate
+ (fn [_ [_ route-id params]]
+   {:pine/update-url (router/path-for route-id params)
+    :dispatch [:pine/handle-navigation]}))
+
+(re-frame/reg-event-fx
+ :pine/handle-url-change
+ (fn [_ [_ url]]
+   {:dispatch [:pine/handle-navigation (router/match-route url)]}))
+
+(re-frame/reg-event-fx
+ :pine/handle-navigation
+ (fn [cofx [_ next-location]]
+   (let [location (:pine/location (:db cofx))
+         [leaving entering retained] (data/diff (:active location) (:active next-location))
+         effects (apply concat (list (map #(vector :leave %) leaving)
+                                     (map #(vector :enter %) entering)
+                                     (map #(vector :retain %) retained)))]
+     {:db (merge (:db cofx) {:pine/location next-location})
+      :dispatch-n effects})))
